b614ed7fe2316577b1e9b6468e8dac62c0a1cd88
Quentin Hausser <hausser.quentin@gmail.com>
2017-01-11 11:50:17 +0100

    Experiments

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -5,28 +5,31 @@ import Html.Events exposing ( onClick )
 -- component import example
 import Components.Hello exposing ( hello )
 
-
 -- APP
-main : Program Never Int Msg
+main : Program Never Model Msg
 main =
   Html.beginnerProgram { model = model, view = view, update = update }
 
 
 -- MODEL
-type alias Model = Int
+type alias Model =
+  {number: Int,
+  elements: List String
+  }
 
-model : number
-model = 0
+model : Model
+model = Model 0 ["Hey Kek"]
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+type Msg = NoOp | Increment | AddElem
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     NoOp -> model
-    Increment -> model + 1
+    Increment -> {model | number = model.number + 1}
+    AddElem -> {model | elements = "Hell yeah" :: model.elements}
 
 
 -- VIEW
@@ -34,21 +37,30 @@ update msg model =
 -- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
+  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][
     div [ class "row" ][
       div [ class "col-xs-12" ][
         div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
+          img [ src "static/img/elm.jpg", style styles.img ] []
+          , hello model.number
           , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
+          , button [ class "btn btn-primary btn-lg", onClick Increment ] [
+            span[ class "glyphicon glyphicon-star" ][]
             , span[][ text "FTW!" ]
           ]
+          , button [ class "btn btn-primary btn-lg", onClick AddElem ] [
+            span[][ text "FTW!" ]
+          ],
+          renderList model.elements
         ]
       ]
     ]
   ]
+
+renderList: List String -> Html Msg
+renderList lst =
+  ul[]
+    (List.map (\element -> li [] [text element]) lst)
 
 
 -- CSS STYLES
